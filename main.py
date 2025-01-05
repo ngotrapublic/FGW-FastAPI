3151650ccb1b0c27aab237e55c2ca405bf97b8dd
ngotra2710 <ngotra27101996@gmail.com>
2025-01-05 20:23:29 +0700

    [TraNLT] cors

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ import uvicorn
 from fastapi import FastAPI
 from src.data.init import create_tables
 from src.web import explorer, creature, user
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    # "*", // Allow all
+]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 create_tables()
 
 app.include_router(explorer.router, tags=["Explorer"])
@@ -11,4 +25,4 @@ app.include_router(creature.router, tags=["Creature"])
 app.include_router(user.router, tags=["User"])
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
