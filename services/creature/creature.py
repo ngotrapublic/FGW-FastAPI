802d4a93b37aa661e827d9d85c41f2b90583f402
ngotra2710 <ngotra27101996@gmail.com>
2025-01-27 19:20:41 +0700

    [TraNLT] kafka

--- services/creature/creature.py
+++ services/creature/creature.py
@@ -1,12 +1,18 @@
+import json
 import uvicorn
 from fastapi import FastAPI
 from src.web import creature
 from fastapi.middleware.cors import CORSMiddleware
+from kafka import KafkaProducer
 
 origins = [
     "http://localhost:8003",
 ]
-
+def json_serializer(data):
+    return json.dumps(data).encode('utf-8')
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+              api_version=(0,11,5),
+              value_serializer = json_serializer)
 app = FastAPI(swagger_ui_parameters={"displayModelsExpandDepth": -1})
 app.add_middleware(
     CORSMiddleware,
@@ -17,6 +23,11 @@ app.add_middleware(
 )
 
 app.include_router(creature.router, tags=["Creature"])
+
+@app.post("/publish/{topic}")
+def publish(topic: str, message: str):
+    producer.send(topic, message)
+    return {"status": "Message published successfully"}
 
 if __name__ == "__main__":
     uvicorn.run("creature:app", reload=True, host="0.0.0.0", port=8001)
--- services/explorer/explorer.py
+++ services/explorer/explorer.py
@@ -1,11 +1,16 @@
+import json
 import uvicorn
 from fastapi import FastAPI
 from src.web import explorer
 from fastapi.middleware.cors import CORSMiddleware
+from kafka import KafkaConsumer
 
 origins = [
     "http://localhost:8003",
 ]
+
+consumer = KafkaConsumer("my_topic", bootstrap_servers='localhost:9092',
+              api_version=(0,11,5))
 
 app = FastAPI()
 app.add_middleware(
@@ -17,6 +22,15 @@ app.add_middleware(
 )
 
 app.include_router(explorer.router, tags=["Explorer"])
+
+@app.get("/consume")
+def consume():
+    messages = []
+    for message in consumer:
+        messages.append(message.value.decode())
+        if len(messages) >= 3:
+            break
+    return {"messages": messages}
 
 if __name__ == "__main__":
     uvicorn.run("explorer:app", reload=True, host="0.0.0.0", port=8002)
