c504320ecd50dcc884e1742d255b2929c493ec01
ngotra2710 <ngotra27101996@gmail.com>
2025-01-28 14:21:34 +0700

    [TraNLT] docker

--- apiGateway/src/data/init.py
+++ apiGateway/src/data/init.py
@@ -2,13 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-user = 'postgres'
-password = '1'
-host = 'localhost'
-port = '5432'
-database = 'postgres'
-connection_str = f'postgresql://{user}:{password}@{host}:{port}/{database}'
-engine = create_engine(connection_str)
+import os
+dbUrl = os.environ['DATABASE_URL']
+# user = 'postgres'
+# password = '1'
+# host = 'localhost'
+# port = '5432'
+# database = 'postgres'
+# connection_str = f'postgresql://{user}:{password}@{host}:{port}/{database}'
+engine = create_engine(dbUrl)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
